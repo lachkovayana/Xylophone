a9460554ac96171c0048aa11df7a2e3df2919df3
Уфимцев Василий Валерьевич <v.ufimtsev@cft.ru>
2021-07-05 17:56:38 +0700

    skeleton

---
+++ app/js/xylophone.mjs
@@ -0,0 +1,36 @@
+let root;
+const KEYS_TO_NOTES = {};
+
+export function init(rootElement) {
+  root = rootElement;
+}
+
+function onBarMouseOver(event) {  
+  
+}
+
+function onBarClick(event) {
+  
+}
+
+function onKeyPress(event) {
+  
+}
+
+function toggleBarDisabledState(bar) {
+  
+}
+
+function isBar(element) { }
+
+function getNoteByBar(bar) { }
+
+function getBarByNote(note) { }
+
+function silenceBar(note) {
+   
+}
+
+function hitBar(bar) {
+   
+}
